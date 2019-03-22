7ebc65693cd45f8af9cfa6aff504fadabb6bda84
bg2345 <bgardynski@gmail.com>
2019-03-21 23:15:19 -0400

    first commit, working on user registration and login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import { Security, ImplicitCallback, SecureRoute } from '@okta/okta-react';
+import Login from './views/login'
+import Home from './views/home'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+       <Security
+         issuer={'https://dev-448350.okta.com'}
+         client_id={'0oadgwwt7VnVuMkba356'}
+         redirect_uri={'http://localhost:3000/implicit/callback'}
+         scope={['openid', 'profile', 'email']}>
+
+         <Switch>
+           <Route exact path="/" component={Login} />
+           <Route path="/implicit/callback" component={ImplicitCallback} />
+           <SecureRoute path="/home" component={Home} />
+         </Switch>
+       </Security>
+     </Router>
     );
   }
 }
